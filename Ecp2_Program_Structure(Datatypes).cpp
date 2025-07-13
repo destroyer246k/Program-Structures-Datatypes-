e712058be32a7af6c693701a09451847784cd657
Patel Vishwas <149381035+destroyer246k@users.noreply.github.com>
2025-07-13 10:14:18 +0530

    Add files via upload

---
+++ Ecp2_Program_Structure(Datatypes).cpp
@@ -0,0 +1,34 @@
+// Patel Vishwas
+// 24070123072
+// A3
+#include<iostream>
+#include<string> // Include the string library to use string data type
+using namespace std;
+int main()
+{
+    // sizeof() is used to find the size of data type in bytes.
+    int a;
+    cout << "Enter integer number:";
+    cin >> a;
+    cout << "Integer is "<< a << " and its size is " << sizeof(a) << " Bytes" << endl<< endl; 
+    float b;
+    cout << "Enter floating number:";
+    cin >> b;
+    cout << "Floating Number is "<< b << " and its size is " << sizeof(b) << " Bytes" << endl<< endl;
+    char c;
+    cout << "Enter Character:";
+    cin >> c;
+    cout << "Character is "<< c << " and its size is " << sizeof(c) << " Bytes " << endl<< endl;
+    double e;
+    cout << "Enter double int number";
+    cin >> e;
+    cout << "double int is "<< e << " and its size is " << sizeof(e) << " Bytes " << endl<< endl;
+    string f;
+    cout << "Enter String ";
+    cin >> f;
+    cout << "string is "<< f << " and its size is " << sizeof(f) << " Bytes " << endl<< endl;
+    bool d;
+    cout << "Enter Boolean value:";
+    cin >> d;
+    cout << "Boolean is "<< d << " and its size is " << sizeof(d) << " Bytes " << endl<< endl;
+}
